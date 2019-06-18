ffae0a195c724ed1d29a943c46d427baed8e2f70
Dylan Teague <dylan.o.teague@vanderbilt.edu>
2019-06-18 14:21:17 -0400

    Implimenting 4top selection. adding wrapper classes as needed

--- interface/TTTSelector.h
+++ interface/TTTSelector.h
@@ -38,15 +38,10 @@ class TTTSelector : public SelectorBase {
   Float_t Mass;
   Float_t MET;
   Float_t type1_pfMETPhi;
-  Float_t l1Phi;
-  Float_t l2Phi;
-  Float_t l3Phi;
-  Float_t l1Mass;
-  Float_t l2Mass;
-  Float_t l3Mass;
 
   //NanoAOD variables
   static const unsigned int N_KEEP_MU_E_ = 10;
+  static const unsigned int N_KEEP_JET_ = 35;
   UInt_t nElectron;
   UInt_t nMuon;
   Float_t Electron_pt[N_KEEP_MU_E_];
@@ -65,48 +60,52 @@ class TTTSelector : public SelectorBase {
   UChar_t Muon_tkIsoId[N_KEEP_MU_E_];
   Float_t Muon_pfRelIso04_all[N_KEEP_MU_E_];
   Int_t numPU;
-    
-  // UWVV variables
-  Float_t nTruePU;
-  UInt_t nCBVIDTightElec;
-  UInt_t nCBVIDHLTSafeElec;
-  UInt_t nWZTightMuon;
-  UInt_t nWZMediumMuon;
-  Bool_t l1IsTight;
-  Bool_t l2IsTight;
-  Bool_t l3IsTight;
-  Float_t l1GenPt;
-  Float_t l2GenPt;
-  Float_t l3GenPt;
-  Bool_t e1IsEB;
-  Bool_t e2IsEB;
-  Bool_t e3IsEB;
-  Float_t e1PVDXY;
-  Float_t e2PVDXY;
-  Float_t e3PVDXY;
-  Float_t e1PVDZ;
-  Float_t e2PVDZ;
-  Float_t e3PVDZ;
-  Float_t m1RelPFIsoDBR04;
-  Float_t m2RelPFIsoDBR04;
-  Float_t m3RelPFIsoDBR04;
-  Float_t l1Eta;
-  Float_t l2Eta;
-  Float_t l3Eta;
-  Float_t l1Pt;
-  Float_t l2Pt;
-  Float_t l3Pt;
-  Float_t l3MtToMET;
-  Float_t Flag_BadChargedCandidateFilterPass;
-  Float_t Flag_BadPFMuonFilterPass;
-  Bool_t Flag_duplicateMuonsPass;
-  Bool_t Flag_badMuonsPass;
-  Bool_t Flag_HBHENoiseFilterPass;
-  Bool_t Flag_HBHENoiseIsoFilterPass;
-  Bool_t Flag_EcalDeadCellTriggerPrimitiveFilterPass;
-  Bool_t Flag_goodVerticesPass;
-  Bool_t Flag_eeBadScFilterPass;
-  Bool_t Flag_globalTightHalo2016FilterPass;
+
+  UInt_t nJet;
+  Float_t Jet_btagCSVV2[N_KEEP_JET_];
+  Float_t Jet_btagDeepB[N_KEEP_JET_];
+  Float_t Jet_eta[N_KEEP_JET_];
+  Float_t Jet_phi[N_KEEP_JET_];
+  Float_t Jet_pt[N_KEEP_JET_];
+  Float_t Jet_mass[N_KEEP_JET_];
+  
+  /* UInt_t nWZTightMuon; */
+  /* UInt_t nWZMediumMuon; */
+  /* Bool_t l1IsTight; */
+  /* Bool_t l2IsTight; */
+  /* Bool_t l3IsTight; */
+  /* Float_t l1GenPt; */
+  /* Float_t l2GenPt; */
+  /* Float_t l3GenPt; */
+  /* Bool_t e1IsEB; */
+  /* Bool_t e2IsEB; */
+  /* Bool_t e3IsEB; */
+  /* Float_t e1PVDXY; */
+  /* Float_t e2PVDXY; */
+  /* Float_t e3PVDXY; */
+  /* Float_t e1PVDZ; */
+  /* Float_t e2PVDZ; */
+  /* Float_t e3PVDZ; */
+  /* Float_t m1RelPFIsoDBR04; */
+  /* Float_t m2RelPFIsoDBR04; */
+  /* Float_t m3RelPFIsoDBR04; */
+  /* Float_t l1Eta; */
+  /* Float_t l2Eta; */
+  /* Float_t l3Eta; */
+  /* Float_t l1Pt; */
+  /* Float_t l2Pt; */
+  /* Float_t l3Pt; */
+  /* Float_t l3MtToMET; */
+  /* Float_t Flag_BadChargedCandidateFilterPass; */
+  /* Float_t Flag_BadPFMuonFilterPass; */
+  /* Bool_t Flag_duplicateMuonsPass; */
+  /* Bool_t Flag_badMuonsPass; */
+  /* Bool_t Flag_HBHENoiseFilterPass; */
+  /* Bool_t Flag_HBHENoiseIsoFilterPass; */
+  /* Bool_t Flag_EcalDeadCellTriggerPrimitiveFilterPass; */
+  /* Bool_t Flag_goodVerticesPass; */
+  /* Bool_t Flag_eeBadScFilterPass; */
+  /* Bool_t Flag_globalTightHalo2016FilterPass; */
     
   std::vector<GoodPart> goodParts;
   
@@ -130,24 +129,29 @@ class TTTSelector : public SelectorBase {
   bool isZgamma_;
   const float FR_MAX_PT_ = 50;
   const float FR_MAX_ETA_ = 2.5;
-  bool zlep1IsTight();
-  bool zlep2IsTight();
-  bool lepton3IsTight();
-  bool tightZLeptons();
-  bool IsGenMatched3l();
-  virtual std::string GetNameFromFile() override;
-  virtual void    SetBranchesNanoAOD() override;
-  virtual void    SetBranchesUWVV() override;
-  void LoadBranchesUWVV(Long64_t entry, std::pair<Systematic, std::string> variation) override;
-  void LoadBranchesNanoAOD(Long64_t entry, std::pair<Systematic, std::string> variation) override;
-  void ApplyScaleFactors();
-  void SetShiftedMasses();
-  void FillHistograms(Long64_t entry, std::pair<Systematic, std::string> variation) override;
-  virtual void    SetupNewDirectory() override;
+  bool IsTightMuon(size_t);
+  bool IsTightElectron(size_t);
+  bool IsTightJet(size_t);
+  bool IsTightBJet(size_t);
+  bool IsOverlap(size_t);
 
-  BranchManager b;
+  int getSRBin();
+  double HT;
+  int nTightJet, nBJets;
   
-};
+  virtual std::string GetNameFromFile() override;
+    virtual void    SetBranchesNanoAOD() override;
+    virtual void    SetBranchesUWVV() override;
+    void LoadBranchesUWVV(Long64_t entry, std::pair<Systematic, std::string> variation) override;
+    void LoadBranchesNanoAOD(Long64_t entry, std::pair<Systematic, std::string> variation) override;
+    void ApplyScaleFactors();
+    void SetShiftedMasses();
+    void FillHistograms(Long64_t entry, std::pair<Systematic, std::string> variation) override;
+    virtual void    SetupNewDirectory() override;
+
+    BranchManager b;
+  
+  };
 
 #endif
 
